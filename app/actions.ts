6091e41820ffa887ccfc99745cba59bd243cfa88
Jonathan Bangert <jonathan@bangert.dk>
2024-11-01 23:04:52 -0600

    Fix a bunch of type errors

--- app/actions.ts
+++ app/actions.ts
@@ -5,14 +5,15 @@ import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export const signUpAction = async (formData: FormData) => {
+export async function signUpAction(formData: FormData): Promise<void> {
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
   const supabase = await createClient();
   const origin = (await headers()).get("origin");
 
   if (!email || !password) {
-    return { error: "Email and password are required" };
+    console.error({ error: "Email and password are required" });
+    return;
   }
 
   const { error } = await supabase.auth.signUp({

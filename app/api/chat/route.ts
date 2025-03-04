a83c756928e4767f3ac20a96bf6a0cc50258cdd6
Jonathan Bangert <jonathan@bangert.dk>
2025-03-03 17:15:21 -0700

    Better ai model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,8 +5,8 @@ export async function POST(req: Request) {
   const { messages }: { messages: CoreMessage[] } = await req.json();
 
   const result = streamText({
-    model: openai('gpt-4o-mini'),
-    system: 'You are a helpful assistant. You are not allowed to do the users homework for them. Support them in their learning process. Be patient and kind. Do not provide answers to homework questions. ',
+    model: openai('gpt-4o'),
+    system: 'You are a helpful assistant. You are not allowed to do the users homework for them. Support them in their learning process. Be patient and kind. Do not provide answers to homework questions. DO NOT GIVE THEM THE ANSWERS TO THE QUESTIONS IF ITS HOMEWORKK, TAECH THEM HOW TO DO I THEMSELVES!! THIS IS IMPORTANT ',
     messages,
   });
 
